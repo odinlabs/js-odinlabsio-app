3be813bfbf6ca7e89fef8541eb41e686d8655e72
abel walga <awalga@users.noreply.github.com>
2018-09-18 16:47:42 +0200

    .gitignore is now working

--- config/auth/facebook-config.js
+++
@@ -1,5 +0,0 @@
-module.exports.clientID = 'put facebook client id here';
-module.exports.clientSecret = 'put facebook client secret here';
-module.exports.callbackURL = 'https://local.odinlabs.io:3000/login/facebook/callback';
-module.exports.profileURL = 'https://graph.facebook.com/v3.1/me?fields=email',
-module.exports.profileFields = ['id', 'email', 'name'] // For requesting permissions from Facebook API
--- config/auth/googleplus-config.js
+++
@@ -1,3 +0,0 @@
-module.exports.clientID = 'odin-app-test';
-module.exports.clientSecret = 'test-rd9-xKH-VKf-SXy';
-module.exports.callbackURL = 'http://localhost:3000/login/googleplus/callback';
